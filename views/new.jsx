8a2fbbdfc12e3d656787f53ecd3c4236d5f00a14
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-26 23:05:43 +0200

    Update new.jsx
    Edited New form to reflect fields in database

--- views/new.jsx
+++ views/new.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function New ({bakers}) {
+function New ({players}) {
+  // confirm we are getting our court data in terminal
+  console.log(players.name)
     return (
       <Default>
-        <h2>Add a new bread</h2>
-        <form action="/breads" method="POST">
+        <h2>Add a new court</h2>
+        <form action="/court" method="POST">
+
           <label htmlFor="name">Name</label>
           <input
             type="text"
@@ -13,28 +16,34 @@ function New ({bakers}) {
             id="name"
             required
           />
+
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            name="Address123"
+            id="Address123"
+            required
+          />
+          
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            required
+          />
+
           <label htmlFor="image">Image</label>
           <input
             type="text"
             name="image"
-            id="image"/>
-            <label htmlFor="baker">Baker</label>
-            <select name="baker" id="baker">
-              {bakers.map((baker) => {
-                return(
-                  <option value={baker.id} key={baker.id}>{baker.name}</option>
-                )
-              })}
-            </select>
-          <label htmlFor="hasGluten">Has Gluten?</label>
-          <input
-            type="checkbox"
-            name="hasGluten"
-            id="hasGluten"
-            defaultChecked
+            id="image"
           />
+
           <br />
+
           <input type="submit"/>
+
         </form>
       </Default>
     )
