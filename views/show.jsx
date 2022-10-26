ce3c75dbc3456287bae198e3372b14e2c8bef781
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-26 19:54:31 +0200

    Update show.jsx
    incorporated court model

--- views/show.jsx
+++ views/show.jsx
@@ -1,29 +1,23 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread, bakedBy}) {
-  // confirm we are getting our bread data in terminal
-  // console.log(bread.name)
+function Show ({court}) {
+  // confirm we are getting our court data in terminal
+  // console.log(court.name)
     return (
       <Default>
-        <h3>{bread.name}</h3>
+        <h3>{court.name}</h3>
         <p>
-          and it
-          {
-            bread.hasGluten
-            ? <span> does </span>
-            : <span> does NOT </span>
-          }
-          have gluten.
+          Located at {`${court.address}, ${court.city}`}
         </p>
-        <img src={bread.image} alt={bread.name} />
+        {/* <img src={bread.image} alt={bread.name} />
         <p>{bread.getBakedBy()}</p>
         <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
         <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
-        </form>
+        </form> */}
 
-        <li><a href="/breads">Go home</a></li>
+        <li className='nobull'><a href="/courts">Go home</a></li>
       </Default>
     )
 }
