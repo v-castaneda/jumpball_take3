ee073bb42c200f69e6029155ae3f3cc62d958bb3
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-12-14 20:33:32 -0500

    creating scss directories and moving original main.css

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,17 +5,7 @@ function Default(html) {
     <html>
     <head>
       <title>{html.title || 'Default'}</title>
-      <link rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" 
-        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
-        crossOrigin="anonymous" 
-      />
-      <link rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" 
-        integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" 
-        crossOrigin="anonymous" 
-      />
-      <link rel="stylesheet" href="/main.css" />
+      <link rel="stylesheet" href="/css/main.css" />
     </head>
     <body>
       <div className="wrapper">
